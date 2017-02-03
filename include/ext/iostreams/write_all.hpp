76430406ff10b1c29a5ce30bf1f0dcba58457749
Dmitry Lisachenko <xdemonx07@gmail.com>
2017-02-03 22:22:31 +0300

    rename blocking_write -> write_all

--- include/ext/iostreams/write_all.hpp
+++ include/ext/iostreams/write_all.hpp
@@ -5,31 +5,29 @@ namespace ext {
 namespace iostreams
 {
 	template <class Sink>
-	inline
-	void blocking_write(Sink & sink, const typename boost::iostreams::char_type_of<Sink>::type * s, std::streamsize n)
+	inline void write_all(Sink & sink, const typename boost::iostreams::char_type_of<Sink>::type * s, std::streamsize n)
 	{
 		//boost::iostreams::write(sink, s, n);
-		while (n) {
+		do {
 			auto res = boost::iostreams::write(sink, s, n);
 			if (res < 0) throw std::ios::failure("failed to write to underline sink");
 
 			s += static_cast<std::size_t>(res);
 			n -= res;
-		};
+		} while (n);
 	}
 
 	template<typename T, typename Sink>
-	inline
-	void blocking_write(T & t, Sink & sink, const typename boost::iostreams::char_type_of<T>::type * s, std::streamsize n)
+	inline void write_all(T & t, Sink & sink, const typename boost::iostreams::char_type_of<T>::type * s, std::streamsize n)
 	{
 		//boost::iostreams::write(t, sink, s, n);
-		while (n) {
+		do {
 			auto res = boost::iostreams::write(t, sink, s, n);
 			if (res < 0) throw std::ios::failure("failed to write to underline sink");
 
 			s += static_cast<std::size_t>(res);
 			n -= res;
-		};
+		} while (n);
 	}
 
 }}
