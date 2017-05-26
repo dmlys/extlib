7226c8c0f441bfbb9c948d31da7150a66e78496d
Dmitry Lisachenko <xdemonx07@gmail.com>
2017-05-26 23:08:31 +0300

    thread_pool::submit(future, func)

--- include/ext/thread_pool.hpp
+++ include/ext/thread_pool.hpp
@@ -21,13 +21,14 @@ namespace ext
 	/// All methods are thread-safe
 	class thread_pool
 	{
+		typedef boost::intrusive::list_base_hook<
+			boost::intrusive::link_mode<boost::intrusive::link_mode_type::normal_link>
+		> hook_type;
+
 	private:
-		/// base interface for submitted tasks, 
+		/// base interface for submitted tasks,
 		/// tasks are hold in intrusive linked list.
-		class task_base : 
-			public boost::intrusive::list_base_hook<
-				boost::intrusive::link_mode<boost::intrusive::link_mode_type::normal_link>
-			>
+		class task_base : public hook_type
 		{
 		public:
 			virtual ~task_base() = default;
@@ -69,6 +70,24 @@ namespace ext
 			friend inline void intrusive_ptr_release(task_impl * ptr) noexcept { ptr->task_release(); }
 			friend inline void intrusive_ptr_use_count(const task_impl * ptr) noexcept {}
 		};
+
+		class delayed_task_continuation :
+			public ext::continuation_base,
+			public hook_type
+		{
+			friend thread_pool;
+
+			thread_pool * m_owner;
+			ext::intrusive_ptr<task_base> m_task;
+
+		public:
+			void continuate() noexcept override;
+			void abandone() noexcept;
+
+		public:
+			delayed_task_continuation(thread_pool * owner, ext::intrusive_ptr<task_base> task)
+				: m_owner(owner), m_task(std::move(task)) {}
+		};
 		
 		/// thread worker object, also a future. When thread is finished - future becomes fulfilled
 		class worker : public ext::shared_state_unexceptional<void>
@@ -92,20 +111,47 @@ namespace ext
 
 	private:
 		typedef ext::intrusive_ptr<worker> worker_ptr;
+		typedef boost::intrusive::base_hook<hook_type> item_list_option;
 
 		typedef boost::intrusive::list<
-			task_base,
+			task_base, item_list_option,
 			boost::intrusive::constant_time_size<false>
 		> task_list_type;
 
+		typedef boost::intrusive::list <
+			delayed_task_continuation, item_list_option,
+			boost::intrusive::constant_time_size<false>
+		> delayed_task_continuation_list;
+
 	private:
-		/// linked list of task 
+		// linked list of task
 		task_list_type m_tasks;
 
-		/// vector of worker objects, it also holds workers that are stopping.
-		/// vector is always partitioned by working/stopping: 
-		///   [0, m_pending)    - working threads
-		///   [m_pending, last) - stopping threads
+		// delayed tasks a little tricky, for every one - we create a service continuation,
+		// which when fired, adds task to task_list.
+		// Those can work and fire when we are destructing or destructed.
+		// thred_pool lifetime should not linger on delayed_task - they should become abandoned.
+		// Nevertheless we have lifetime problem.
+		//
+		// so we store those active service continuations in a list:
+		//  - When continuation is fired it checks if it's taken:
+		//    * if yes - thread_pool is gone, nothing to do;
+		//    * if not - thread_pool is still there and we should add task to a list;
+		// 
+		// - Destructor enumerates them and marks them as taken:
+		//   * if successful - service continuation is sort of cancelled and it will not access thread_pool
+		//   * if not - continuation is firing right now somewhere in the middle,
+		//     so destructor must wait until it finishes and then complete destruction.
+		delayed_task_continuation_list m_delayed;
+
+		// how many delayed_continuations were not "taken/cancelled" at destruction,
+		// and how many we must wait - it's sort of a semaphore.
+		std::size_t m_delayed_count = 0;
+
+		// vector of worker objects, it also holds workers that are stopping.
+		// vector is always partitioned by working/stopping:
+		//   [0, m_pending)    - working threads
+		//   [m_pending, last) - stopping threads
 		std::vector<worker_ptr> m_workers;
 		std::size_t m_pending = 0;
 
@@ -130,11 +176,11 @@ namespace ext
 		/// sets number of workers:
 		/// if n == $current - does nothing and returns ready future
 		/// if n >  $current - creates more workers and returns ready future
-		/// if n <  $current - stops $current - n workers and returns future, 
+		/// if n <  $current - stops $current - n workers and returns future,
 		///                   which become ready when all stopped workers are completely stopped.
 		ext::future<void> set_nworkers(unsigned n);
 
-		/// stops all workers, returns future which become ready when all workers, 
+		/// stops all workers, returns future which become ready when all workers,
 		/// that were created in these thread pool at any time,
 		/// are completely stopped.
 		ext::future<void> stop();
@@ -145,7 +191,11 @@ namespace ext
 		auto submit(Functor && func) ->
 			ext::future<std::result_of_t<std::decay_t<Functor>()>>;
 		
-		/// clears all not already executed tasks. 
+		template <class Future, class Functor>
+		auto submit(Future future, Functor && func) ->
+			ext::future<std::result_of_t<std::decay_t<Functor>(Future)>>;
+
+		/// clears all not already executed tasks.
 		/// Associated futures status become abandoned
 		void clear();
 
@@ -178,5 +228,32 @@ namespace ext
 
 		m_event.notify_one();
 		return fut;
+	}
+
+	template <class Future, class Functor>
+	auto thread_pool::submit(Future future, Functor && func) ->
+		ext::future<std::result_of_t<std::decay_t<Functor>(Future)>>
+	{
+		auto handle = future.handle();
+		auto wrapped = [func = std::forward<Functor>(func), future = std::move(future)]() mutable
+		{
+			return func(std::move(future));
+		};
+
+		typedef decltype(wrapped) functor_type;
+		typedef std::result_of_t<functor_type()> result_type;
+		typedef task_impl<functor_type, result_type> task_type;
+		typedef ext::future<result_type> future_type;
+
+		auto task = ext::make_intrusive<task_type>(std::move(wrapped));
+		auto cont = ext::make_intrusive<delayed_task_continuation>(this, task);
+		
+		{
+			std::lock_guard<std::mutex> lk(m_mutex);
+			m_delayed.push_back((cont.addref(), *cont.get()));
+		}
+
+		handle->add_continuation(cont.get());
+		return future_type {std::move(task)};
 	}
 }
--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -23,6 +23,42 @@ namespace ext
 		return m_stop_request.exchange(true, std::memory_order_relaxed);
 	}
 
+	void thread_pool::delayed_task_continuation::continuate() noexcept
+	{
+		auto owner = m_owner;
+		bool notify;
+
+		if (not mark_taken())
+			// thread_pool is destructed or destructing
+			return;
+
+		// remove ourself from m_delayed and add m_task to thread_pool tasks list
+		{
+			std::lock_guard<std::mutex> lk(owner->m_mutex);
+			
+			auto & list = owner->m_delayed;
+			auto & delayed_count = owner->m_delayed_count;
+			auto it = list.iterator_to(*this);
+			list.erase(it);
+
+			owner->m_tasks.push_back(*m_task.release());
+			notify = delayed_count == 0 || --delayed_count == 0;
+		}
+		
+		// notify thread_pool if needed
+		if (notify) owner->m_event.notify_one();
+
+		// we were removed from m_delayed - intrusive list,
+		// which does not manage lifetime, decrement refcount
+		release();
+	}
+
+	void thread_pool::delayed_task_continuation::abandone() noexcept
+	{
+		m_task->task_abandone();
+		m_task = nullptr;
+	}
+
 	unsigned thread_pool::get_nworkers() const
 	{
 		std::lock_guard<std::mutex> lk(m_mutex);
@@ -106,7 +142,7 @@ namespace ext
 
 		for (;;)
 		{
-			ext::intrusive_ptr<task_base> task_ptr; 
+			ext::intrusive_ptr<task_base> task_ptr;
 			lk.lock();
 
 			if (stop_request.load(std::memory_order_relaxed)) return;
@@ -151,27 +187,55 @@ namespace ext
 	thread_pool::~thread_pool() noexcept
 	{
 		// can std::atomic_memory_fence(std::memory_order_seq_cst) used?
-
+		
 		std::vector<worker_ptr>::iterator first, last;
 		task_list_type tasks;
+		delayed_task_continuation_list delayed;
+
 		{
 			std::lock_guard<std::mutex> lk(m_mutex);
-			tasks.swap(m_tasks);
+			delayed.swap(m_delayed);
 			first = m_workers.begin();
 			last = m_workers.end();
 		}
 
+		// stopping threads
 		for (auto it = first; it != last - m_pending; ++it)
 			(**it).stop_request();
 
+		// wake threads if they are sleeping/waiting
 		m_event.notify_all();
 
+		// cancel/take delayed tasks, see thread_pool body description
+		assert(m_delayed_count == 0);
+		for (auto it = delayed.begin(); it != delayed.end();)
+		{
+			if (not it->mark_taken())
+				++m_delayed_count;
+			else
+			{
+				auto & item = *it;
+				it = delayed.erase(it);
+				item.abandone();
+				item.release();
+			}
+		}
+
+		// wait until all delayed_tasks are finished, and take pending tasks
+		{
+			std::unique_lock<std::mutex> lk(m_mutex);
+			m_event.wait(lk, [this] { return m_delayed_count == 0; });
+			tasks.swap(m_tasks);
+		}
+
+		// abandone them
 		tasks.clear_and_dispose([](task_base * task)
 		{
 			task->task_abandone();
 			task->task_release();
 		});
 
+		// wait until threads are stopped
 		for (auto it = first; it != last; ++it)
 			(**it).wait();
 	}
