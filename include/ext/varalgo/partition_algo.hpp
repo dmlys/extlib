99a2917275b9db5db78d4714bb302bc12b21da2c
Dmitry Lisachenko <xdemonx07@gmail.com>
2017-09-28 01:35:49 +0300

    moved varalgo -> viewed

--- include/ext/varalgo/partition_algo.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <ext/varalgo/is_partitioned.hpp>
-#include <ext/varalgo/partition.hpp>
-#include <ext/varalgo/partition_copy.hpp>
-#include <ext/varalgo/stable_partition.hpp>
-#include <ext/varalgo/partition_point.hpp>
-
