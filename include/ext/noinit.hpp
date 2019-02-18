3864b3c39ca4d8095888bd09af6843343b14af2b
Dmitry Lisachenko <xdemonx07@gmail.com>
2019-02-19 01:02:35 +0300

    added symbol  ext::noinit

---
+++ include/ext/noinit.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+namespace ext
+{
+	/// Special type/value used as indication that initialization should not be done,
+	/// usually used as an constructor argument, but can be used in other contexts too(for example vector:resize(n, ext::noinit).
+	/// What initialization is - is defined by each class itself
+	struct noinit_type {} constexpr noinit {};
+}
