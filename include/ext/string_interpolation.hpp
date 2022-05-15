2322adc0123ac8a032a502b2ddec11282a180f5f
Dmitry Lisachenko <xdemonx07@gmail.com>
2022-05-16 01:16:52 +0300

    refactored and improved ext::string_interpolation

--- include/ext/string_interpolation.hpp
+++ include/ext/string_interpolation.hpp
@@ -4,42 +4,76 @@
 #include <utility>  // for std::pair
 #include <tuple>    // for std::tie
 
+#include <ext/is_string.hpp>
 #include <ext/range/range_traits.hpp>
-#include <boost/range/as_literal.hpp>
+#include <ext/range/str_view.hpp>
 
-/// функции интерполяции строк подобно unix shell оболочкам,
-/// но допустимые символы обрабатываются несколько упрощенно
-/// ${var} для имени допустимы любые символы
-/// $var допустимы только 0-9a-zA-Z_
-///  $, $0, $_, $var
-/// поддерживается escape'инг \$var, $var\ cont, ${var\}}
+#include <boost/mp11/list.hpp>
+#include <boost/mp11/utility.hpp>
+
+/// string interpolation functions similar to unix shells,
+/// but allowed symbols are handled somewhat simplistically
+/// ${var} forms allows any symbols
+/// $var for allows only 0-9a-zA-Z_
+///  $0, $_, $var
+/// symbol escaping is supported \$var, $var\ cont, ${var\}}
+/// No extended forms of expansion(bash like or any other) is supported (for example ${VAR:-12})
 /// 
-/// пример использование:
+/// usage example:
 ///     std::map<string, string> dict = {{"name", "bond"}, {"date", "friday"}};
-///     string res = ext::interpolate("my name is %name and this is test on %date val%", dict);
+///     string res = ext::interpolate("my name is $name and this is test on $date", dict);
 /// 
-/// Dictionary - класс словаря:
-///   * должен иметь как минимум key_type typedef - тип ключа,
-///     некоторый строковый stl-like container совместимый с входной строкой
-///   * должна быть глобальная функция dictionary_find:
-///     auto res = dictionary_find(dict, key);
-///     результат - некоторый тип:
-///       - конвертируемый в bool, если данные по ключу найдены - true, иначе false
-///       - при разыменование - возвращает некий string range того же типа что и входной текст
-///         например, это может быть указатель(как простой так и smart), boost::optional
+/// Dictionary - dictionary object:
+///	  * can be std::map like - should have key_type, mapped_type typedefs
+///     find begin, end methods
+///   * invokable functor, taking std::basic_string as searchable argument,
+///     returning std::optional<char container> as result
 ///
-/// входной range любой boost::range совместимый range символов.
-/// Поддерживаемые типы символов: char/char16_t/char32_t/wchar_t.
-/// выходной параметр итератор или контейнер.
-/// для контейнера должны быть специализированна функция ext::append.
+/// input range can be any boost compatible char range
+/// supported char types: char/char16_t/char32_t/wchar_t.
+/// result can be written into output iterator or output container.
+///   for container ext::append function call should be valid
 
 namespace ext
 {
-	template <class StdMapType, class Key>
-	const typename StdMapType::mapped_type * dictionary_find(const StdMapType & map, const Key & key);
+	/// interpolates range with given dictionary into dest iterator
+	template <class Range, class Dictionary, class OutIter>
+	std::enable_if_t<not ext::is_range_v<OutIter>, OutIter>
+	interpolate(const Range & text, const Dictionary & dict, OutIter dest);
 
+	/// interpolates range with given dictionary into dest container
+	template <class Range, class Dictionary, class Container>
+	std::enable_if_t<ext::is_container_v<Container>, void>
+	interpolate(const Range & text, const Dictionary & dict, Container & outc);
+
+	/// interpolates range with given dictionary and returning as std::string
+	template <class Range, class Dictionary>
+	auto interpolate(const Range & text, const Dictionary & dict) ->
+		std::basic_string<ext::range_value_t<Range>>;
+	
+	/// interpolates range with given dictionary and returning as std::string
+	template <class CharT, class CharTraits, class Allocator, class Dictionary>
+	auto interpolate(const std::basic_string<CharT, CharTraits, Allocator> & text, const Dictionary & dict)
+		-> std::basic_string<CharT, CharTraits, Allocator>;
+	
+	
+	/************************************************************************/
+	/*                   implementation                                     */
+	/************************************************************************/
 	namespace interpolate_internal
 	{
+		template <class Type>
+		using get_key_type = typename Type::key_type;
+		
+		template <class Type>
+		using get_mapped_type = typename Type::mapped_type;
+		
+		template <class Type>
+		struct has_key_type : boost::mp11::mp_valid<get_key_type, Type> {};
+		
+		template <class Type>
+		struct has_mapped_type : boost::mp11::mp_valid<get_mapped_type, Type> {};
+		
 		/// карта допустимых символов для переменных в не расширенном формате.
 		/// это 0-9 a-z A-Z _
 		extern const bool key_allowed_ascii_chars[128];
@@ -57,19 +91,17 @@ namespace ext
 		};
 		
 		struct key_stop
-		{
-			template <class Char,
-				typename std::enable_if<std::is_signed<Char>::value, int>::type = 0
-			>
-			inline bool operator()(Char ch) const
+		{			
+			template <class Char>
+			std::enable_if_t<std::is_signed_v<Char>, bool>
+			operator()(Char ch) const
 			{
 				return operator()(typename std::make_unsigned<Char>::type(ch));
 			}
 
-			template <class Char,
-				typename std::enable_if< ! std::is_signed<Char>::value, int>::type = 0
-			>
-			inline bool operator()(Char ch) const
+			template <class Char>
+			std::enable_if_t<std::is_unsigned_v<Char>, bool>
+			operator()(Char ch) const
 			{
 				return ch > 127 || !key_allowed_ascii_chars[ch];
 			}
@@ -102,12 +134,28 @@ namespace ext
 		}
 		
 		
+				
+		/// implementation dictionary_find for std::map like classes
+		template <class Dictionary, class Key>
+		auto dictionary_find(const Dictionary & dict, const Key & key) -> const get_mapped_type<Dictionary> *
+		{
+			auto it = dict.find(key);
+			return it != dict.end() ? &it->second : nullptr;
+		}
+		
+		/// implementation dictionary_find for functions and alike
+		template <class Dictionary, class Key>
+		auto dictionary_find(const Dictionary & dict, const Key & key) -> std::invoke_result_t<const Dictionary, const Key>
+		{
+			return dict(key);
+		}
+		
 		template <class Iterator, class OutIterator, class Dictionary>
 		OutIterator interpolate_into_iterator(Iterator first, Iterator last, OutIterator out, const Dictionary & dict)
 		{
-			typedef typename std::iterator_traits<Iterator>::value_type char_type;
-			typedef typename Dictionary::key_type key_type;
-
+			using char_type = typename std::iterator_traits<Iterator>::value_type;
+			using key_type = boost::mp11::mp_eval_or<std::basic_string<char_type>, get_key_type, Dictionary>;
+			
 			key_type key;
 			for (;;)
 			{
@@ -119,7 +167,8 @@ namespace ext
 				
 				++first;
 				// после символа ключа - конец строки. копируем символ и выходим
-				if (first == last) {
+				if (first == last)
+				{
 					*out++ = '$';
 					return out;
 				}
@@ -155,12 +204,11 @@ namespace ext
 				}
 				
 				/// выполняем подстановку
-				using ext::dictionary_find;
-				auto val = dictionary_find(dict, key);
+				auto val = ext::interpolate_internal::dictionary_find(dict, key);
 				if (val)
 					out = std::copy(boost::const_begin(*val), boost::const_end(*val), out);
 				else
-				{   // ключ 'отклонено' - копируем имя ключа как есть из шаблона
+				{   // ключ 'отклонен' - копируем имя ключа как есть из шаблона
 					*out++ = '$';
 					if (extended) *out++ = '{';
 					out = std::copy(boost::const_begin(key), boost::const_end(key), out);
@@ -172,8 +220,8 @@ namespace ext
 		template <class Iterator, class Containter, class Dictionary>
 		void interpolate_into_container(Iterator first, Iterator last, Containter & cont, const Dictionary & dict)
 		{
-			typedef typename std::iterator_traits<Iterator>::value_type char_type;
-			typedef typename Dictionary::key_type key_type;
+			using char_type = typename std::iterator_traits<Iterator>::value_type;
+			using key_type = boost::mp11::mp_eval_or<std::basic_string<char_type>, get_key_type, Dictionary>;
 
 			key_type key;
 			auto outit = std::back_inserter(cont);
@@ -187,7 +235,8 @@ namespace ext
 
 				++first;
 				// после символа ключа - конец строки. копируем символ и выходим
-				if (first == last) {
+				if (first == last)
+				{
 					*outit++ = '$';
 					return;
 				}
@@ -223,8 +272,7 @@ namespace ext
 				}
 				
 				/// выполняем подстановку
-				using ext::dictionary_find;
-				auto val = dictionary_find(dict, key);
+				auto val = ext::interpolate_internal::dictionary_find(dict, key);
 				if (val)
 					ext::append(cont, boost::const_begin(*val), boost::const_end(*val));
 				else
@@ -238,58 +286,43 @@ namespace ext
 		}
 	} // namespace InterpolateInternal
 
-	/// поиск в карте словаре по умолчанию,
-	/// может быть специализированно/перегружено, в том числе в родном namespace'е класса -
-	/// тем самым предоставляя поиск в словаре для interpolate функций
-	template <class StdMapType, class Key>
-	const typename StdMapType::mapped_type * dictionary_find(const StdMapType & map, const Key & key)
+	/// interpolates range with given dictionary into dest iterator
+	template <class Range, class Dictionary, class OutIter>
+	inline std::enable_if_t<not ext::is_range_v<OutIter>, OutIter>
+	interpolate(const Range & text, const Dictionary & dict, OutIter dest)
 	{
-		auto it = map.find(key);
-		return it != map.end() ? &it->second : nullptr;
-	}
-
-	template <
-		class Range, class Dictionary, class OutIter,
-		typename std::enable_if<!ext::is_range<OutIter>::value, int>::type = 0
-	>
-	inline OutIter interpolate(const Range & text, const Dictionary & dict, OutIter dest)
-	{
-		static_assert(std::is_same<char, typename boost::range_value<Range>::type>::value, "only char currently supported");
-		auto textlit = boost::as_literal(text);
-		return interpolate_internal::interpolate_into_iterator
+		auto textlit = ext::str_view(text);
+		return ext::interpolate_internal::interpolate_into_iterator
 			(boost::const_begin(textlit), boost::const_end(textlit), dest, dict);
 	}
 
-	template <
-		class Range, class Dictionary, class Container,
-		typename std::enable_if<ext::is_range<Container>::value, int>::type = 0
-	>
-	inline void interpolate(const Range & text, const Dictionary & dict, Container & outc)
+	/// interpolates range with given dictionary into dest container
+	template <class Range, class Dictionary, class Container>
+	inline std::enable_if_t<ext::is_container_v<Container>, void>
+	interpolate(const Range & text, const Dictionary & dict, Container & outc)
 	{
-		static_assert(std::is_same<char, typename boost::range_value<Range>::type>::value, "only char currently supported");
-		auto textlit = boost::as_literal(text);
-		interpolate_internal::interpolate_into_container
+		auto textlit = ext::str_view(text);
+		ext::interpolate_internal::interpolate_into_container
 			(boost::const_begin(textlit), boost::const_end(textlit), outc, dict);
 	}
 
+	/// interpolates range with given dictionary and returning as std::string
 	template <class Range, class Dictionary>
-	auto interpolate(const Range & text, const Dictionary & dict) ->
-	                 std::basic_string<typename boost::range_value<Range>::type>
+	inline auto interpolate(const Range & text, const Dictionary & dict) ->
+		std::basic_string<ext::range_value_t<Range>>
 	{
 		std::basic_string<typename boost::range_value<Range>::type> res;
-		interpolate(text, dict, res);
+		ext::interpolate(text, dict, res);
 		return res;
 	}
-
-	template <
-		class CharT, class CharTraits, class Allocator,
-		class Dictionary
-	>
-	auto interpolate(const std::basic_string<CharT, CharTraits, Allocator> & text,
-	                 const Dictionary & dict) -> std::basic_string<CharT, CharTraits, Allocator>
+	
+	/// interpolates range with given dictionary and returning as std::string
+	template <class CharT, class CharTraits, class Allocator, class Dictionary>
+	inline auto interpolate(const std::basic_string<CharT, CharTraits, Allocator> & text, const Dictionary & dict)
+		-> std::basic_string<CharT, CharTraits, Allocator>
 	{
 		std::basic_string<CharT, CharTraits, Allocator> res;
-		interpolate(text, dict, res);
+		ext::interpolate(text, dict, res);
 		return res;
 	}
 }
